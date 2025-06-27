c9f5ce6d48724df75ce858da6bfeda84bd161ef8
TTEesstt26 <konstantinkudinov648@gmail.com>
2025-06-27 14:36:09 +0600

    Add files via upload

---
+++ practice2025/task01/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+
+            string palindrome = string.Empty;
+            foreach (char c in input.ToLower())
+            {
+                if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+                {
+                    palindrome += c;
+                }
+            }
+
+            if (string.IsNullOrEmpty(palindrome))
+            {
+                return false;
+            }
+
+            string reversed = new string(palindrome.Reverse().ToArray());
+            return palindrome == reversed;
+        }
+    }
+}
